f18b38f5e9060278a639681ae50ea06191c54a74
Kai%20wei Nan <kaiweinan@yahoo.com>
2020-11-03 22:17:57 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,3 +13,11 @@ $(document).ready(function(){
       $("img").addClass("tasty");
     });
   }
+
+  function pressIt(){
+    $("input").on('keydown'function(e){
+      if (e.which==71){
+        alert('G was pressed');
+      }
+    });
+  }
