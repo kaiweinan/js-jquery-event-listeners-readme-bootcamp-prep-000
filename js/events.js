6c6e1bdabb18ff74efcf4abc49b255a06921adfb
Kai%20wei Nan <kaiweinan@yahoo.com>
2020-11-03 22:17:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
 });
+
   function getIt() {
     $('p').on("click", function () {
       alert("Hey!");
@@ -10,13 +11,5 @@ $(document).ready(function(){
   function frameIt(){
     $("img").on("load", function(){
       $("img").addClass("tasty");
-    });
-  }
-
-  function pressIt(){
-    $("input").on('keydown'function(e){
-      if (e.which==71){
-        alert('G was pressed');
-      }
     });
   }
