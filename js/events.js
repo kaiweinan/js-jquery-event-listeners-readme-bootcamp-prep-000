347472bae57deedc52fa0a2f5280c37d592a2feb
Kai%20wei Nan <kaiweinan@yahoo.com>
2020-11-03 22:22:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -25,7 +25,8 @@ $(document).ready(function(){
 function submitIt(){
   $("form").on("submit",function(){
     if ($("input:first").val()==="correct"){
-      alert("Your form is going to be submitted now.")
+      alert("Your form is going to be submitted now.");
+      return;
     }
   })
 }
